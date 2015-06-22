6c569a1010c25c958f5d08838bb522cf85bc73e1
Said Pelaez <diego.210@hotmail.es>
2015-06-22 01:43:59 -0500

    Rabbits content, times, class

--- README.md
+++ README.md
@@ -39,6 +39,16 @@ Finally match routes
 rabbit.match();
 ```
 
+## Is it magic?
+Nope, it's not so you'll have to specify your project's folder name to make sure rabbits work properly
+```
+rabbit.project_name = "your_project_folder";
+```
+### Why?
+When you're working with a server like xampp your "/" route will be "htdocs" NOT your project directory so, by doing this rabbits
+will be able to look for your files.
+
+
 ## Adding and changing error responses
 In rabbits you'll be able to update or add errors catching functions for *_404_* and *_500_*
 ```
@@ -61,4 +71,47 @@ rabbit.set_error_codes(error_codes);
 	<p>[JSON_obj.name]</p>
 	<img src="[JSON_obj.path]" alt="[JSON_obj.name]">
 </div>
+```
+
+## Rabbits class
+1. Use this function to quickly add classes to an element
+```
+<div rbt-class="(boolean) true : false">
+	content
+</div>
+```
+
+## Rabbits Content
+Ok, this is one of the most interesting functions yet, so there I go.
+Rabbits content is a function thought to make html simpler using js, with this you'll be able to use "partials" this way
+```
+<div rbt-content="name_of_file"></div>
+```
+Or you can also specify a route
+```
+<div rbt-content="path/to/file.ext"></div>
+```
+### rbt-content error handler
+If you get an error using this you could handle it by adding the following error codes to your "error_codes" arrays
+	01.400 Bad request
+	01.404 Not found
+	01.500 Server Error
+
+## Rabbits times
+This one is easy, is a little module that allows you to repeat the same code many times
+```
+<div rbt-times="10">Hello</div>
+```
+will render as
+```
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello
+hello
 ```
--- rabbits.js
+++ rabbits.js
@@ -1,5 +1,5 @@
 /*
-	Rabbits, by Notion. 0.1a
+	Rabbits, by Notion. 0.0.4a
 	Rabbits it's a JS based (As you already may know) routes manager, that's it :3
 	
 	CC-BY, Notion
@@ -34,6 +34,9 @@ var rabbitsModule = function (r) {
 	this.padding = 3; // Distance until where rabbits it's going to start to read.
 	this.error_code = null;
 	this.rabbits_elements = [];
+	this.project_name = "rabbits";
+	this.procedure = [];
+	this.execution_list = [];
 	this.error_codes = [
 		{code: 404, action: function(){alert("This is not the page you're looking for... ");} }
 	];
@@ -74,7 +77,7 @@ var rabbitsModule = function (r) {
 
 		for (var i = 0; i < this.routes.length; i++) { // Go through the routes
 			var compiled = "'" + path + "'.match(" + this.regex_routes[i] + ")"; // Build the match
-			console.log(compiled);
+			//console.log(compiled);
 			if( eval(compiled) != null ){ // The route matched?
 				if (i != 0 || path == "/") {
 					has_match = true;
@@ -90,7 +93,7 @@ var rabbitsModule = function (r) {
 				}else{
 					var local = [];
 					for (var j = 0; j < this.routes_obj[i].params_indices.length; j++) {
-						console.log(this.routes_obj[i].params_indices[j][1]);
+						//console.log(this.routes_obj[i].params_indices[j][1]);
 						window.params[ this.routes_obj[i].params_indices[j][1] ] = window.location.pathname.split("/")[this.routes_obj[i].params_indices[j][0] + (this.padding - 1)];
 						local.push(window.location.pathname.split("/")[this.routes_obj[i].params_indices[j][0] + (this.padding - 1)] );
 					};
@@ -139,7 +142,7 @@ var rabbitsModule = function (r) {
 
 	/* This little function is used to redirect things when you're using rabbits */
 	this.push = function(title, url, callback){
-		history.pushState(null, title, "/barbarossa/r"+url);
+		history.pushState(null, title, "/" + this.project_name + "/r"+url);
 		callback();
 	}
 	
@@ -172,68 +175,169 @@ var rabbitsModule = function (r) {
 			self = elements[i];
 			while(attr = elements[i].attributes[j++]){
 		  		var tmp = attr.name; // Use a tmp variable to save the value of the attribute because it's an object
-			    if (eval("'" + tmp + "'.match(/rabbit-*/g)")) { // check regex
+
+			    if (eval("'" + tmp + "'.match(/(rabbit|rbt)-*/g)")) { // check regex
 			    	tmp = tmp.substr(tmp.indexOf("-") + 1, tmp.length - 1); // Here we get the function name and so on
+			    	//console.log( typeof attr.value, attr.value );
 			    	this.rabbits_elements.push(self);
-			    	eval("this." + tmp + "(" + (this.rabbits_elements.length - 1) + "," + attr.value + ")"); // We execute the function with it's value as a parameter
+			    	//console.log(tmp);
+			    	this.procedure.push(tmp);
+			    	this.execution_list.push("this.r_" + tmp + "(" + (this.rabbits_elements.length - 1) + ",'" + attr.value + "')"); // We execute the function with it's value as a parameter
 			    }
 			    	
 			}
 		};
+
+		this.execute();
 	}
 
-	this.init = function(){
-		this.function_tail( this.get_elements_by_attr("/rabbit-*/g") );
+	this.execute = function(){
+		for (var i = 0; i < this.execution_list.length; i++) {
+			this.execution_list[i] = this.execution_list[i].replace(/\'/g, "\'");
+			eval(this.execution_list[i]);
+		};
 	}
 
-	this.repeat = function(index, json){
+	this.init = function(callback){
+		this.function_tail( this.get_elements_by_attr("/(rabbit|rbt)-*/g") ); // Get elements with rbt or rabbit attr
+		if (typeof callback != "undefined")
+			callback();
+	}
+
+	this.r_times = function(index, json){
+		var element = this.rabbits_elements[index];
+		var content = element.innerHTML;
+		var output = "";
+		var _times = parseInt(json);
+		console.log(content);
+		content = content.replace(/\n/g, "");
+		for (var i = 0; i < _times; i++) {
+			var _tmp = content.replace(/\{\$index\}/g, i);
+			output += _tmp;
+		};
+		element.innerHTML = output;
+	};
+
+	this.r_class = function(index, json){  // You use this as rbt-class="(condition) true : false" :)
+		var condition = "";
+		var end = /\)/g;
+		var start = /\(/g;;
+		end.test(json);
+		start.test(json);
+		var condition = json.substring(start.lastIndex, end.lastIndex - 1);
+		var _class = json.substring(end.lastIndex + 1, json.length);
+		var elemente = this.rabbits_elements[index];
+
+		if ( eval(condition) ) {
+			this.rabbits_elements[index].className = _class.split(" : ")[0];
+		}else{
+			if ( _class.indexOf(":") != -1 ) {
+				_class = _class.split(" : ")[1];
+				this.rabbits_elements[index].className = _class;
+			};
+		};
+	}
+
+	this.raise = function(ex){
+		for (var i = 0; i < this.error_codes.length; i++) {
+			if( this.error_codes[i].code == ex ){
+				this.error_codes[i].action();
+				break;
+			};
+		};
+	}
+
+	this.r_content = function(index, json){
+		var xmlhttp;
+		var path = "";
+		var element = this.rabbits_elements[index];
+		var self = this;
+	    if (window.XMLHttpRequest) {
+	        // code for IE7+, Firefox, Chrome, Opera, Safari
+	        xmlhttp = new XMLHttpRequest();
+	    } else {
+	        // code for IE6, IE5
+	        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	    }
+
+		if (json.indexOf(".") != -1) { // if a file extension is specified
+			path = json;
+		}else{
+			path = "_" + json + ".html";			
+		};
+
+		console.log(path);
+	    xmlhttp.open("GET", path, true);
+
+		xmlhttp.onreadystatechange = function() {
+	        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+	           switch(xmlhttp.status){
+	            	case 200:
+	            		element.innerHTML = xmlhttp.responseText;
+	            	break;
+	            	
+	            	case 400:
+	            		self.raise(1.400);
+	            	break;
+
+	            	case 404:
+	            		self.raise(1.404);
+	            	break;
+
+	            	case 500:
+	            		self.raise(1.500);
+	            	break;
+	           }
+	        }
+	    }
+	    
+	    xmlhttp.send();
+
+	}
+
+	this.r_repeat = function(index, json){
+		var json = eval(json);
 		var template = this.rabbits_elements[index].innerHTML;
 		template = template.replace(/\t/g, "");
 		var element = this.rabbits_elements[index];
-		var num_of_params = 
 		element.innerHTML = "";
+
 		for (var i = 0; i < json.length; i++) {
 			var tmp = template;
-			for (var j = 0; j < template.length; j++) {
-				if(template[j] == "]"){  // 
-					var found = false;
-					
-					for (var k = j; k >= 0; k--) {
-						
-						if( template[k] == "["){
-							var part = template.substring(k+1, j );
-							part = part.replace(/\s/g, "");
-							console.log(part);
-							
-							var part_ = part.substring(0, part.indexOf("."));
-
-							var _part = part.substring(part.indexOf(".") + 1, part.length);
-							_part = _part.replace(/\]/g, "");
-
-							console.log(eval("json["+i+"]." + _part));
-
-							tmp = tmp.replace(eval("/\\[" + part_ + "\\." + _part + "\\]/g"), eval("json["+i+"]." + _part));
-							console.log(tmp, k, j, i);;;;
-							
-							found = true;
-							k = 0;
-						}
-						
-					};
-				}
-				if(!found){
-					this.error_code = 500;
-					console.log("Could not find the start of the sentece, check your code");
-				}
-			};
-			//element.innerHTML += template;
+			tmp = tmp.replace(/\n/g, "");
+			var end = /\]/g;
+			var start = /\[/g;
+			var times = tmp.match(end).length;
+			for(var index = 0; index < times; index++){
+				end = /\]/g;
+				start = /\[/g;
+				//console.log(tmp);
+				end.test(tmp);
+				var j = end.lastIndex;
+				start.test(tmp);
+				var k = start.lastIndex;
+				//console.log(tmp[j-1], tmp[k-1], index);
+				var found = false;
+				var part = tmp.substring(k-1, j);
+				part = part.replace(/\s/g, "");
+				//console.log(part, "in -> ", index, "i -> ", i);			
+				var part_ = part.substring(1, part.indexOf("."));
+				var _part = part.substring(part.indexOf(".") + 1, part.length);
+				_part = _part.replace(/\]/g, "");
+				//console.log(eval("/\\[" + part_ + "\\." + _part + "\\]/g"), eval("json["+i+"]." + _part), "->", i);
+				//console.log("-> ", eval("json["+i+"]." + _part));
+				tmp = tmp.replace(/\{\$index\}/g, "" + i);
+				tmp = tmp.replace(eval("/\\[" + part_ + "\\." + _part + "\\]/g"), eval("json["+i+"]." + _part));
+				
+				//console.log(tmp)
+			}
 			element.innerHTML += tmp;
-		};
+		}
 	}
 }
 
 /* This little function is used to redirect things when you're using rabbits */
 var push = function(title, url, callback) {
-	history.pushState(null, title, "/barbarossa/r"+url);
+	history.pushState(null, title, "/" + this.project_name + "/r" + url);
 	callback();
 }
